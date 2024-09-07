ea4e92c461a3959db5bb0241cc44e38e3f959c44
Rodney Mort <rodneymort@gmail.com>
2024-09-07 10:54:29 -0700

    Remove unneeded HTTPClient

--- Program.cs
+++ Program.cs
@@ -8,6 +8,4 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 await builder.Build().RunAsync();
